25f37f98ca2c90fbf51c1b1829e0b3cd58568e77
asafonov <me@asafonov.org>
2020-09-21 17:14:40 +0300

    Added controllers to compile

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 > main.js
 for i in `ls models`; do cat models/$i >> main.js; done
+for i in `ls controllers`; do cat controllers/$i >> main.js; done
 for i in `ls view`; do cat view/$i >> main.js; done
 cat globals.js >> main.js
 cat init.js >> main.js
